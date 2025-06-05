90151d0100911bf2a8094111546803a1e36379ac
DamianVillalba <devillalba20@gmail.com>
2025-06-04 23:14:24 -0300

    feat: creo CartIcon para transferir logica contador y logo carrito de header

---
+++ src/components/static/CartIcon.tsx
@@ -0,0 +1,20 @@
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
+interface CartIconProps{
+    countCart : number;
+}
+
+const CartIcon = ({countCart} : CartIconProps) => {
+	return (
+		<div className="relative">
+			<ShoppingCartIcon className="h-8 w-8 text-gray-700" />
+			{countCart > 0 && (
+				<span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center shadow-md">
+					{countCart}
+				</span>
+			)}
+		</div>
+	);
+};
+
+export default CartIcon;
--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,16 +1,12 @@
 "use client";
 import { Link } from "react-router-dom";
-import {
-	PopoverGroup
-} from "@headlessui/react";
-import {
-	ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
-import Cart from '../Cart';
+import { PopoverGroup } from "@headlessui/react";
+import Cart from "../Cart";
 import { useCart } from "../../context/CartContext";
+import CartIcon from "./CartIcon";
 
 export default function Header() {
-	const { showCart, toggleCart } = useCart();
+	const { cart, showCart, toggleCart } = useCart();
 
 	return (
 		<header className="bg-amber-100">
@@ -19,7 +15,7 @@ export default function Header() {
 				className="mx-auto flex max-w-7xl items-center justify-between py-4 px-6 lg:px-8"
 			>
 				<div className="flex lg:flex-1">
-					<Link to='/' className="-m-1.5 p-1.5">
+					<Link to="/" className="-m-1.5 p-1.5">
 						<span className="sr-only">React Talento-Tech</span>
 						<img
 							alt="Home"
@@ -29,24 +25,33 @@ export default function Header() {
 					</Link>
 				</div>
 				<PopoverGroup className="hidden lg:flex lg:gap-x-12">
-					<Link to='/products' className="text-sm/6 font-semibold text-gray-900">Productos</Link>
-					<Link to="/about-us" className="text-sm/6 font-semibold text-gray-900">Acerca de</Link>
-					<Link to="/contact" className="text-sm/6 font-semibold text-gray-900">Contacto</Link>
+					<Link
+						to="/products"
+						className="text-sm/6 font-semibold text-gray-900"
+					>
+						Productos
+					</Link>
+					<Link
+						to="/about-us"
+						className="text-sm/6 font-semibold text-gray-900"
+					>
+						Acerca de
+					</Link>
+					<Link to="/contact" className="text-sm/6 font-semibold text-gray-900">
+						Contacto
+					</Link>
 				</PopoverGroup>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-2">
-                    <button
-                        onClick={toggleCart}
-                        className="flex items-center gap-2 px-3 py-1 rounded hover:cursor-pointer"
-                        aria-label="Abrir carrito"
-                    >
-                        <ShoppingCartIcon aria-hidden="true" className="h-8 w-auto" />
-                        <span className="sr-only">Abrir carrito</span>
-                    </button>
-                </div>
-            </nav>
-            {showCart && (
-                <Cart />
-            )}
+				<div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-2">
+					<button
+						onClick={toggleCart}
+						className="flex items-center gap-2 px-3 py-1 rounded hover:cursor-pointer"
+						aria-label="Abrir carrito"
+					>
+						<CartIcon countCart = {cart.length} />
+					</button>
+				</div>
+			</nav>
+			{showCart && <Cart />}
 		</header>
 	);
 }
